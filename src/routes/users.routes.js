2798693796636d89f3e0db057e830db78f99fd1e
Gabriel Riomar Lopes <gabrielriomar@gmail.com>
2023-04-12 15:44:35 -0300

    pratica backend

---
+++ src/routes/users.routes.js
@@ -0,0 +1,19 @@
+const { Router } = require('express')
+
+const UsersController = require('../controllers/UsersController')
+
+const usersRoutes = Router()
+
+// function myMiddleware(request, response, next){
+//   console.log("Você passou pelo middleware")
+//   console.log(request.body)
+
+//   next()
+// }
+
+const usersController = new UsersController()
+
+usersRoutes.post('/', usersController.create)
+usersRoutes.put('/:id', usersController.update)
+
+module.exports = usersRoutes
