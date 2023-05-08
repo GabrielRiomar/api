0538a22dcc0f3f322d43ad69052aa929449f3c2a
Gabriel Riomar Lopes <gabrielriomar@gmail.com>
2023-05-08 19:01:56 -0300

    user authentication and avatar img upload

--- src/routes/index.js
+++ src/routes/index.js
@@ -3,11 +3,13 @@ const { Router } = require('express')
 const usersRoutes = require('./users.routes.js')
 const notesRoutes = require('./notes.routes.js')
 const tagsRoutes = require('./tags.routes.js')
+const sessionsRoutes = require('./sessions.routes.js')
 
 const routes = Router()
 
 routes.use('/users', usersRoutes)
 routes.use('/notes', notesRoutes)
 routes.use('/tags', tagsRoutes)
+routes.use('/sessions', sessionsRoutes)
 
 module.exports = routes
